82b5357a43e0fcc11d4ac7e9fab60b0c56581bdb
oswaldopaulo <oswaldo.paulo@gmail.com>
2023-03-01 21:22:22 -0300

    API

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,9 +40,14 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+//            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+//            'throttle:api',
+//            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+
         ],
     ];
 
