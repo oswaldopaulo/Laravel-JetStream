dfe93a50bead0452fd68332d282f234b24184bb9
oswaldopaulo <oswaldo.paulo@gmail.com>
2023-10-27 08:22:43 -0300

    fix

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,31 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
-
 use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Validator;
-
 class ApiController extends Controller
 {
     //
     public function get(){
          return response()->json(['msg'=>'ok',200]);
     }
-
     public function post(Request $r){
         return response()->json([$r->all()],201);
     }
 
     public function update(Request $r,$id){
-
-
-        return response()->json(['id'=>$id],201);
+        return response()->json(['id'=>$id],200);
     }
     public function delete(Request $r,$id){
-
-
-        return response()->json(['id'=>$id],201);
+        return response()->json(['id'=>$id],200);
     }
 
 }
