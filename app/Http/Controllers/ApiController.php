85e3aa802b5df2df6dc622f5de2c8549d6139f09
oswaldopaulo <oswaldo.paulo@gmail.com>
2023-10-27 08:41:03 -0300

    fix

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,16 +4,13 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Request;
 class ApiController extends Controller
 {
-    //
-    public function get(){
+     public function get(){
          return response()->json(['msg'=>'ok'],200);
     }
     public function post(){
         return response()->json([Request::all()],201);
     }
-
     public function update($id){
-
         return response()->json(['id'=>$id, Request::all()],200);
     }
     public function delete($id){
